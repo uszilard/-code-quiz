a83fb3386c03a7d516d60ca70fa80cbc3fc7cea6
Szilard Udroiu <uszilard@gmail.com>
2019-10-16 22:07:21 +0200

    commit

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+  {
+    title: "What does HTML stand for:",
+    choices: ["Home Tool Markup Language", "Hyperlinks and Text Markup Language", "Hyper Text Markup Language"],
+    answer: "Hyper Text Markup Language"
+  },
+  {
+    title: "Choose the correct HTML element for the largest heading:",
+    choices: ["<h6>", "<h1>", "<heading>", "<head>"],
+    answer: "<h1>"
+  },
+  {
+    title: "Which HTML attribute is used to define inline styles?",
+    choices: ["styles", "font", "class", "style"],
+    answer: "style"
+  },
+  {
+    title: "Which CSS property controls the text size?",
+    choices: ["font-size", "text-style", "text-size", "font-style"],
+    answer: "font-size"
+  },
+  {
+    title: "Where is the correct place to insert a JavaScript?",
+    choices: ["The <body> section", "Both the <head> section and the <body> section are correct", "The <head> section", "None of the above"],
+    answer: "Both the <head> section and the <body> section are correct"
+  }
+];
